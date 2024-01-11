1be2e195c052b679ea04a97160245f4ec546d25b
banbarossa <banbarossa@gmail.com>
2024-01-11 15:32:57 +0700

    updat gambar di piket2

--- resources/views/livewire/piket/kelola-absen-mengajar.blade.php
+++ resources/views/livewire/piket/kelola-absen-mengajar.blade.php
@@ -67,9 +67,9 @@
                                         </td>
                                         <td>
                                             @if ($item->image)
-                                            <a href="{{asset('/storage/public/images'.$item->image)}}" class="thumbnail-link">
+                                            {{-- <a href="{{asset('/storage/public/images'.$item->image)}}" class="thumbnail-link">
                                                 <img src="{{asset('/storage/public/images/'.$item->image)}}" alt="Thumbnail Image" class="thumbnail">
-                                            </a>
+                                            </a> --}}
                                             <a href="{{asset('storage/public/images/'.$item->image)}}" class="thumbnail-link">
                                                 <img src="{{asset('storage/public/images/'.$item->image)}}" alt="Thumbnail Image" class="thumbnail rounded-circle">
                                             </a>
