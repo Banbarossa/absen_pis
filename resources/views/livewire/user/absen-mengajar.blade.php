2827e4c91f76bacf7e2c3cdac4bebfe2d4bf722d
banbarossa <banbarossa@gmail.com>
2024-02-08 11:26:18 +0700

    Perbaiki Tampilan di bagian Absen Mengajar2

--- resources/views/livewire/user/absen-mengajar.blade.php
+++ resources/views/livewire/user/absen-mengajar.blade.php
@@ -68,7 +68,7 @@
                                                         <td>
                                                             @if ($item->kehadiran == 'alpa' && !$item->complainmengajar && $item->akhir_kbm < $now)
                                                             <button class="btn btn-warning" wire:click='complain({{$item->id}})' data-toggle="modal" data-target="#crudModal">Complain</button>
-                                                            @elseif ($item->kehadiran != 'alpa' && !$item->complainmengajar && $item->akhir_kbm < $now)
+                                                            @elseif ($item->kehadiran != 'alpa' && !$item->complainmengajar)
                                                             ✔️
                                                             @else
                                                             Belum Absen
