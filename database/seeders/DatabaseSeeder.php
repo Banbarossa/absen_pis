cbbbecfd93b65d9db15014e4951105ebb3f65bab
banbarossa <banbarossa@gmail.com>
2024-02-17 20:34:38 +0700

    Perbaiki seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use Database\Seeders\SecureLocationSeeder;
-use Database\Seeders\UserSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,21 +14,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(40)->create();
-        $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::class);
         $this->call(RoleSeeder::class);
         // \App\Models\Sekolah::factory(3)->create();
-        $this->call(SekolahSeeder::class);
-        $this->call(RombelSeeder::class);
+        // $this->call(SekolahSeeder::class);
+        // $this->call(RombelSeeder::class);
         // \App\Models\Rombel::factory(12)->create();
 
-        $this->call(SemesterSeeder::class);
-        $this->call(ScheduleSeeder::class);
+        // $this->call(SemesterSeeder::class);
+        // $this->call(ScheduleSeeder::class);
 
         // \App\Models\Jammengajar::factory(100)->create();
         // $this->call(JammengajarSeeder::class);
-        $this->call(MapelSeeder::class);
+        // $this->call(MapelSeeder::class);
 
-        $this->call(JadwalHalaqahSeeder::class);
+        // $this->call(JadwalHalaqahSeeder::class);
         // \App\Models\Absenhalaqah::factory(100)->create();
 
         // $this->call(AksesabsenSeeder::class);
@@ -38,7 +36,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\Absensekolah::factory(200)->create();
         // \App\Models\Absenhalaqah::factory(200)->create();
 
-        $this->call(SecureLocationSeeder::class);
+        // $this->call(SecureLocationSeeder::class);
 
     }
 }
